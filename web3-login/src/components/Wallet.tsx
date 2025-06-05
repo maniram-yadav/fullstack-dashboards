5d482fc96069d0dce8e018334931e9ba4a029620
maniram <maniramyadav786@gmail.com>
2025-06-05 20:05:51 +0530

    web 3 login implemenation

---
+++ web3-login/src/components/Wallet.tsx
@@ -0,0 +1,54 @@
+'use client'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
+export default function Wallet() {
+    
+    const [account, setAccount] = useState<string | null>(null)
+    const [balance, setBalance] = useState<string | null>(null)
+
+    const isMetaMaskInstalled = () => typeof window !== 'undefined' && typeof window.ethereum !== 'undefined'
+
+    const connectWallet = async () => {
+        if (!isMetaMaskInstalled()) {
+            alert('metamask is not detecetd. Install it first')
+            return
+        }
+        try {
+            const provider = new ethers.BrowserProvider(window.ethereum)
+            const accounts = await provider.send('eth_requestAccounts', [])
+            const address = accounts[0]
+            setAccount(address)
+            const balanceBigInt = await provider.getBalance(address)
+            setBalance(ethers.formatEther(balanceBigInt))
+        } catch (error) {
+            console.log('Connection error ', error)
+        }
+    }
+
+    const disconnectWallet = () => {
+        setAccount(null)
+        setBalance(null)
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen
+         bg-gray-900 text-white p-4">
+            {!account ? (
+                <button onClick={connectWallet} className="px-3 py-3 rounded-lg text-white bg-indigo-600
+                hover:bg-indigo-700 min-h-screen pointer-cursor"> Login</button>
+            ) :
+                <div>
+                    <button onClick={disconnectWallet} className="px-3 py-3 rounded-lg text-white bg-indigo-600
+                hover:bg-indigo-700 min-h-screen  pointer-cursor"> Logout</button>
+                    <div>
+                        {account}
+                    </div>
+                    <div>
+                        {balance}
+                    </div>
+                </div>
+            }
+        </div>
+    )
+}
