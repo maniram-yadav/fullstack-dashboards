4e885e4384cb3e73e3c75fbe5f4a2bba038b194e
maniram <maniramyadav786@gmail.com>
2025-07-04 14:27:21 +0530

    navbar menu added

--- tailwindcss/index1.js
+++ tailwindcss/index1.js
@@ -24,3 +24,46 @@ sections.forEach(section => {
     console.log(section);
     sectionObserver.observe(section);
 });
+
+
+
+
+
+
+
+function toggleMenu(){
+            const menu = document.getElementsByClassName("menu");
+            console.log(menu)
+            console.log(menu[0].classList)
+            menu[0].classList.toggle('hidden');
+}
+
+// typewrite code
+const textToType = "Slack\nGithub\nEmail";
+const typewriterElement = document.getElementById("trail");
+let charIndex = 0;
+const typingSpeed = 200; 
+const length = textToType.length
+
+function typeWriter() {
+    
+        if (charIndex==length){
+            
+            typewriterElement.textContent = "";
+            charIndex=0;
+            setTimeout(typeWriter, 1000);
+            
+        } else if (charIndex < textToType.length) {
+            
+            // typewriterElement.innerHTML += "<div>";
+            typewriterElement.textContent += textToType.charAt(charIndex);
+            // typewriterElement.innerHTML += "</div>";
+            charIndex++;
+            setTimeout(typeWriter, typingSpeed);
+        }
+            
+    
+}
+
+// Start the typewriter effect when the page loads
+window.onload = typeWriter;
